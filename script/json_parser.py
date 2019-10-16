873ebd22b1b1d259a7762b79d01d8ed1488ce1c4
Kaida Yushi <kaidayushi@gmail.com>
2019-10-17 05:04:54 +0900

    create parser tool

---
+++ script/json_parser.py
@@ -0,0 +1,54 @@
+import os
+import json
+
+def json_parser(name):
+  a = open(name)
+  b = json.load(a)
+  
+  labels = []
+  
+  for i, j in zip(b["shapes"], range(len(b["shapes"]))):
+    rect = i["points"]
+    size_y = rect[1][0] - rect[0][0]
+    size_x = rect[1][1] - rect[0][1]
+    centre_y = (rect[1][0] + rect[0][0]) / 2
+    centre_x = (rect[1][1] + rect[0][1]) / 2
+    label_class = i["label"].split("-")[0]
+    label = { u"size": {
+                 u"y": size_y,
+                 u"x": size_x
+              },
+              u"label_type": u"box",
+              u"label_class": label_class,
+              u"object_id": j + 1,
+              u"centre": {
+                 u"y" : centre_y,
+                 u"x" : centre_x
+              }
+            }
+    labels.append(label)
+  
+  root, _ = os.path.splitext(name)
+  image_filename = root.split("./")[1] + ".jpg"
+  parsered_json = {"labels": labels,
+                  "complete": None,
+                  "image_filename": image_filename}
+  
+  parseredFileName = root + "__labels_to.json"
+  c = open(parseredFileName, 'w')
+  json.dump(parsered_json, c, indent=4)
+
+
+if __name__ == "__main__":
+  for curDir, dirs, files in os.walk("."):
+    for f in files:
+      fileName = os.path.join(curDir, f)
+      root, ext = os.path.splitext(fileName)
+      if ext == ".json" and not("__labels" in root):
+<<<<<<< HEAD
+=======
+        print(ext)
+        print(fileName)
+>>>>>>> 322a331563a9e3aff427e8d28cdcd764d994ba1c
+        json_parser(fileName)
+
